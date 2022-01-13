d443b1d1b2bb2f11b8c90ec832658bec20526e34
ozzielo <57558395+ozzielo@users.noreply.github.com>
2022-01-12 23:23:15 -0500

    Create

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello World!1111
+
         </p>
         <a
           className="App-link"
